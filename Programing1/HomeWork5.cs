f872e46d4af38b194302c63cc29f1abe712152da
0x0sp1d3r <42107203+0x0sp1d3r@users.noreply.github.com>
2021-03-20 00:03:41 +0100

    adding emmpty methods for the upcoming problems/homeworks

---
+++ Programing1/HomeWork5.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace Programing1
+{
+    public class HomeWork5
+    {
+        public void SwitchRowsArray() // 6.1
+        {
+
+            /*
+            deklarera en vektor med 4 rader, 4 kolumner.
+            fyll i den med metoden random.
+            byt rad nummer 2 med rad numer 3.
+            Skriv ut den nya vektor.
+            */
+
+
+
+
+
+        }
+
+        public void LettersInCharCounter() // 6.2
+        {
+
+            /*
+            Läs en mening från tangentbordet.
+            Konvertera den i en array ("char[] myString = Console.ReadLine().ToCharArray();" ).
+            Räkna hur många gånger förekommer en visst bokstav i den mening
+            (bokstaven läser du in från tangentbordet).
+            Skriv det på skärmen.
+            */
+
+
+
+
+        }
+
+        public void InputFromUserRepostionArray() //6.3
+        {
+
+            /*
+            Skapa en vektor med 21 platser.
+            Fyll i dem första 20 platserna med metoden Random.
+            Fråga användare på vilken plats o vilken element ska man lägga i
+            (T. Ex, på plats 5, nr. 432). Flytta alla elementen en position till höger
+            o lägg i den nya elementen på rätt plats.
+            Skriv ut den nya vektor på skärmen.
+            */
+
+
+
+
+
+        }
+
+        public void MaxMinArray()
+        {
+
+            /*
+            deklarera en vektor med 20 platser, o fyll den med Random metoden,
+            med siffror mellan 1 till 10.000.
+            Hitta därefter min o max i den vektor o skriv den på skärmen
+            */
+
+
+
+
+        }
+
+
+
+        public HomeWork5()
+        {
+        }
+    }
+}
