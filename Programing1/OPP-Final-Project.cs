a5cdb4fbc695f3a3b5c4a8a5f26e5dd5624fbc53
0x0sp1d3r <42107203+0x0sp1d3r@users.noreply.github.com>
2021-04-11 20:49:07 +0200

    Wrote the logic of three helper funcations AddPlayer(), RemovePlayer() and PrintPlayers()

--- Programing1/OPP-Final-Project.cs
+++ Programing1/OPP-Final-Project.cs
@@ -25,14 +25,15 @@ namespace Programing1
     public class Final_Project_Run
     {
 
-        Players[] tabel = new Players[10];
-        readonly int ageLimit = 18;
         
-        void Run() // Head of Project You call this to trigger the rest. 
+        public static void  Run() // Head of Project You call this to trigger the rest. 
         {
+            Console.Clear();
+
+            Players[] tabel = new Players[10];
+            int ageLimit = 18;
 
             int Option = 1;
-            
             while (Option != 0)
             {
                 Console.Write("This is the tabel of players, what would you like to do :\n" +
@@ -42,8 +43,24 @@ namespace Programing1
                               "4- Add a Player to the tabel.\n" +
                               "5- Remove a Player from the tabel.\n" +
                               "0- To Exit The Table App.\n");
-
-                Option = Convert.ToInt32(Console.ReadLine());
+                while (true)
+                {
+                    try
+                    {
+                        Option = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    }
+                    catch(FormatException)
+                    {
+                        Console.WriteLine("Please Enter a Number from the Menu");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                        
+                    }
+                }
+                
 
                 switch (Option)
                 {
@@ -80,150 +97,237 @@ namespace Programing1
 
             }
 
-
-        }
-
-        // Defining Methods
-        void SearchPlayers()
-        {
-
-
-        }
-
-        void PrintPlayers()
-        {
-
-
-        }
-
-        void AverageAge()
-        {
-
-
-
-        }
-
-        void AddPlayers()
-        {
-            string inputName;
-            while (true) // taking Name input
+            // Defining Methods
+            void SearchPlayers()
             {
-                try
-                {
-                    Console.WriteLine("Enter The Name Of the Player: \n");
-                    inputName = Console.ReadLine();
-                    break;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                }
+                Console.Clear();
+
+
+
+                Console.WriteLine("--------------------------------------------------");
             }
 
-            int inputAge;
-            while (true) // taking Age input
+            void PrintPlayers()
             {
-                try
+                /*
+                for (int i = 0; i < tabel.Length; i++)
                 {
-                    Console.WriteLine("Enter the Age Of the Player: \n");
-                    inputAge = Convert.ToInt32(Console.ReadLine());
-                    break;
+                    Console.WriteLine("Player : {0}, Age: {1}, Time: {3}");
                 }
-                catch (FormatException)
+                */
+                Console.Clear();
+                int place = 0;
+                foreach (Players player in tabel)
                 {
-                    Console.WriteLine("Please Enter a number!!");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                }
-            }
-
-            double inputTime;
-            while (true) // taking Record/Time input
-            {
-                try
-                {
-                    Console.WriteLine("Enter the Player's Record/Time: \n");
-                    inputTime = Convert.ToDouble(Console.ReadLine());
-                    break;
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Please Enter a double!!");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                }
-            }
-
-            Console.WriteLine("Press (y) to add the player at a certain place. Or Press (n) to add it at the first empty place.\n");
-            ConsoleKeyInfo option = Console.ReadKey(true);
-            switch (option.Key)
-            {
-                case ConsoleKey.Y:
-                    int pos;
-                    pos = Convert.ToInt32(Console.ReadLine());
-                    if (tabel[pos - 1] == null) // check if the index is empty 
+                    place++;
+                    if (player == null)
                     {
+                        Console.WriteLine("{0}: This Row {1} is Empty", place, place);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0}: Player : {1}, Age: {2}, Time: {3}.", place, player.Name, player.Age, player.Time);
+                    }
+                }
+                Console.WriteLine("--------------------------------------------------");
+            }
+
+            void AverageAge()
+            {
+                Console.Clear();
+
+
+                Console.WriteLine("--------------------------------------------------");
+            }
+
+            void AddPlayers()
+            {
+                Console.Clear();
+                // taking Name input
+                string inputName;
+                while (true) 
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter The Name Of the Player: \n");
+                        inputName = Console.ReadLine();
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+
+                // taking Age input
+                int inputAge;
+                while (true) 
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter the Age Of the Player: \n");
+                        inputAge = Convert.ToInt32(Console.ReadLine());
+                        break;
                         
-                        tabel[pos - 1] = new Players(inputName, inputAge, inputTime);
                     }
-                    else // in case the index isn't empty, to avoid overwriting info
+                    catch (FormatException)
                     {
-                        for (int i = 0; i < tabel.Length - 1; i++)
-                        {
-                            if (tabel[i] == null)
-                            {
-                                tabel[i] = new Players(inputName, inputAge, inputTime);
-                                break;
-                            }
-                            else
-                            {
-                                continue;
-                            }
-
-                        }
+                        Console.WriteLine("Please Enter a Number!!");
                     }
-                    break;
-
-                case ConsoleKey.N:
-
-                    for (int i = 0; i < tabel.Length - 1; i++)
+                    catch (Exception ex)
                     {
-                        if (tabel[i] == null)
-                        {
-                            tabel[i] = new Players(inputName, inputAge, inputTime);
+                        Console.WriteLine(ex);
+                    }
+                }
+
+                // taking Record/Time input
+                double inputTime;
+                while (true) 
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter the Player's Record/Time: \n");
+                        inputTime = Convert.ToDouble(Console.ReadLine());
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Please Enter a Number!!");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+
+
+                if (inputAge >= ageLimit)
+                {
+                    // Give the user an option to choose the place if empty 
+                    Console.WriteLine("Press (y) to add the player at a certain place. Or Press (n) to add it at the first empty place.\n");
+                    ConsoleKeyInfo option = Console.ReadKey(true);
+                    switch (option.Key)
+                    {
+                        case ConsoleKey.Y:
+                            int pos;
+                            while (true) // taking input of the Postion/Place {pos}
+                            {
+                                try
+                                {
+                                    Console.WriteLine("Enter the place where you want to place the player: \n");
+                                    pos = Convert.ToInt32(Console.ReadLine());
+                                    break;
+                                }
+                                catch (FormatException)
+                                {
+                                    Console.WriteLine("Please Enter a Number");
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex);
+                                }
+                            }
+
+
+                            if (tabel[pos - 1] == null) // check if the index is empty 
+                            {
+                                tabel[pos - 1] = new Players(inputName, inputAge, inputTime);
+                            }
+                            else // in case the index isn't empty, to avoid overwriting info
+                            {
+                                for (int i = 0; i < tabel.Length - 1; i++) // Linear Search for an empty place
+                                {
+                                    if (tabel[i] == null)
+                                    {
+                                        tabel[i] = new Players(inputName, inputAge, inputTime);
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Row {0}, is full. Trying next row.", i);
+                                        continue;
+                                    }
+
+                                }
+                            }
+
                             break;
-                        }
-                        else
-                        {
-                            continue;
-                        }
+
+                        case ConsoleKey.N:
+
+                            for (int i = 0; i < tabel.Length - 1; i++)
+                            {
+                                if (tabel[i] == null)
+                                {
+                                    tabel[i] = new Players(inputName, inputAge, inputTime);
+                                    Console.WriteLine("Player Has been Added at row {0}", i);
+                                    break;
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+
+                            }
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid Option, try again!");
+                            break;
 
                     }
-                    break;
+                }
+                else
+                {
+                    Console.WriteLine("The Player is too young to participate in the Tournament!!!.\n");
+                }
 
-                default:
-                    Console.WriteLine("Invalid Option, try again!");
-                    break;
+
+
+
+                Console.WriteLine("--------------------------------------------------");
             }
 
-           
+            void RemovePlayers()
+            {
+                Console.Clear();
+                PrintPlayers(); // print the current tabel of players info 
+                try
+                {
+                    Console.WriteLine("Choose the row to clear thus delete the player from the Table ");
+                    int row = Convert.ToInt32(Console.ReadLine()) - 1;
 
+                    if (row < 0 || row > tabel.Length)
+                    {
+                        Console.WriteLine("Please Choose a number between 1 and {0}", tabel.Length);
+                    }
+                    else if (tabel[row] == null)
+                    {
+                        Console.WriteLine("The Row is alraedy empty, no player info is found");
+                    }
+                    else
+                    {
+                        Console.WriteLine("PLayer: {0} at Row: {1} has been Deleted!", tabel[row].Name, row);
+                        tabel[row] = null;
+                    }
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Please Enter a Number!!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+                Console.WriteLine("--------------------------------------------------");
+            }
 
 
         }
 
-        void RemovePlayers()
-        {
 
 
-
-        }
-
-        
 
     }
 
